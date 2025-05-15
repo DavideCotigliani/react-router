75d0ae85365a466347a0f21de4b6b8e72685f134
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-15 14:34:20 +0200

    [UPD] Created the components in App.js to manage the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={HomePage}></Route>
+          <Route path="/chisiamo" Component={ChiSiamo}></Route>
+          <Route path="/prodotti" Component={Prodotti}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
