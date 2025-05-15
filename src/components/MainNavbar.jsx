961c7d093eb3251b493d4d496a422962bdc50dfc
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-15 16:54:33 +0200

    [UPD] used the map to cycle the NavLink

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,14 +1,34 @@
 import { NavLink } from "react-router-dom"
-
+const links = [
+    {
+        path: '/',
+        label: 'Homepage',
+    },
+    {
+        path: '/chisiamo',
+        label: 'Chisiamo',
+    },
+    {
+        path: '/prodotti',
+        label: 'Prodotti',
+    }
+]
 const MainNavbar = () => {
     return (
         <div>
             <header>
                 <nav>
                     <ul>
-                        <li><NavLink className="custom-link" to="/">HomePage</NavLink></li>
+                        {links.map((link, index) => {
+                            return (
+                                <li key={index}>
+                                    <NavLink className="custom-link" to={link.path}>{link.label}</NavLink>
+                                </li>
+                            )
+                        })}
+                        {/* <li><NavLink className="custom-link" to="/">HomePage</NavLink></li>
                         <li><NavLink className="custom-link" to="/chisiamo">Chi Siamo</NavLink></li>
-                        <li><NavLink className="custom-link" to="/prodotti">Prodotti</NavLink></li>
+                        <li><NavLink className="custom-link" to="/prodotti">Prodotti</NavLink></li> */}
                     </ul>
                 </nav>
             </header>
